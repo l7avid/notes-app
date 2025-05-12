6675a5fd524bea753ab59136464dcd91f285ef3f
David Reina <davidereina@hotmail.com>
2025-05-12 18:19:39 -0500

    improving coverage

--- __tests__/adapters/ui/screens/HomePage.spec.tsx
+++ __tests__/adapters/ui/screens/HomePage.spec.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import React from 'react';
+import { Alert } from 'react-native';
 import { useSelector } from 'react-redux';
+import { act } from 'react-test-renderer';
 import { HomePage } from '../../../../src/adapters/ui/screens/HomePage';
 import * as fetchNotes from '../../../../src/lib/fetchAllNotes';
-// Mocks
+import { supabase } from '../../../../src/lib/supabase';
+import navigationScreenNames from '../../../../src/utils/constants/navigationScreenNames';
+
 jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
 }));
@@ -36,9 +40,9 @@ jest.mock(
           <Text>Delete</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => props.onEdit('Edited Content')}
-          testID="edit-button">
-          <Text>Edit</Text>
+          onPress={() => props.onEdit('Content edited')}
+          testID={`edit-button-${props.note.id}`}>
+          <Text>{`text-${props.note.content}`}</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={props.onShare} testID="share-button">
           <Text>Share</Text>
@@ -101,7 +105,7 @@ describe('HomePage', () => {
         shared_by_username: 'username',
         content: 'Test note',
         created_at: '',
-        id: 'test-note-1',
+        id: 'note-1',
         profile_id: 'profile1',
         shared_by: 'user1',
         profiles: {
@@ -118,8 +122,7 @@ describe('HomePage', () => {
     expect(getByTestId('signout-page')).toBeTruthy();
     expect(getByTestId('user-list-modal')).toBeTruthy();
 
-    // Waiting for the note to render
-    expect(await findByText('Test note')).toBeTruthy();
+    expect(await findByText('text-Test note')).toBeTruthy();
   });
 
   it('adds a new note when submitted', async () => {
@@ -127,10 +130,8 @@ describe('HomePage', () => {
       <HomePage navigation={{replace: jest.fn()}} />,
     );
 
-    // Trigger add note form submission
     fireEvent.press(getByTestId('add-note-form'));
 
-    // Waiting for the new note to appear in the list
     expect(await findByText('New Note')).toBeTruthy();
   });
 
@@ -140,10 +141,8 @@ describe('HomePage', () => {
     );
     await waitFor(() => expect(getByTestId('delete-button')).toBeTruthy());
 
-    // Trigger delete action
     fireEvent.press(getByTestId('delete-button'));
 
-    // Waiting for the note to be removed from the list
     await waitFor(() => expect(queryByText('Test note')).toBeNull());
   });
 
@@ -151,12 +150,12 @@ describe('HomePage', () => {
     const {getByTestId, findByText} = render(
       <HomePage navigation={{replace: jest.fn()}} />,
     );
-    await waitFor(() => expect(getByTestId('edit-button')).toBeTruthy());
-    // Trigger edit action
-    fireEvent.press(getByTestId('edit-button'));
 
-    // Waiting for the edited content to appear
-    expect(await findByText('Edited Content')).toBeTruthy();
+    await waitFor(() => expect(getByTestId('edit-button-note-1')).toBeTruthy());
+
+    fireEvent.press(getByTestId('edit-button-note-1'));
+
+    expect(await findByText('text-Content edited')).toBeTruthy();
   });
 
   it('opens share modal when share is clicked', async () => {
@@ -165,10 +164,150 @@ describe('HomePage', () => {
     );
 
     await waitFor(() => expect(getByTestId('share-button')).toBeTruthy());
-    // Trigger share action
     fireEvent.press(getByTestId('share-button'));
 
-    // Waiting for the modal to open
     expect(getByTestId('user-list-modal')).toBeTruthy();
+  });
+
+  it('redirects to login if userInfo is null', () => {
+    const mockReplace = jest.fn();
+    (useSelector as unknown as jest.Mock).mockReturnValue(null);
+
+    render(<HomePage navigation={{replace: mockReplace}} />);
+    expect(mockReplace).toHaveBeenCalledWith(navigationScreenNames.LOGIN);
+  });
+
+  it('shows alert if note submission fails', async () => {
+    const alertSpy = jest.spyOn(Alert, 'alert');
+    const mockInsert = jest.fn(() => ({
+      select: jest.fn().mockResolvedValue({
+        data: [{note_id: null}],
+        error: {message: 'Insert failed'},
+      }),
+    }));
+
+    jest.mocked(supabase.from).mockImplementation(
+      () =>
+        ({
+          insert: mockInsert,
+        } as any),
+    );
+
+    const {getByTestId} = render(
+      <HomePage navigation={{replace: jest.fn()}} />,
+    );
+    fireEvent.press(getByTestId('add-note-form'));
+
+    await waitFor(() => {
+      expect(alertSpy).toHaveBeenCalledWith('Server Error', 'Insert failed');
+    });
+  });
+
+  it('shows alert if deleting a note fails', async () => {
+    jest.spyOn(fetchNotes, 'fetchUserNotes').mockResolvedValue([
+      {
+        shared_by_username: 'username',
+        content: 'Test note',
+        created_at: '',
+        id: 'test-note-1',
+        profile_id: 'profile1',
+        shared_by: 'user1',
+        profiles: {
+          username: 'username',
+        },
+      },
+    ]);
+
+    const alertSpy = jest.spyOn(Alert, 'alert');
+
+    const mockDelete = jest.fn(() => ({
+      eq: jest.fn().mockResolvedValue({
+        data: null,
+        error: {message: 'Delete failed'},
+      }),
+    }));
+
+    const mockFrom = jest.fn(() => ({
+      delete: mockDelete,
+    }));
+
+    const supabaseMock = require('../../../../src/lib/supabase');
+    supabaseMock.supabase.from = mockFrom;
+
+    const {getByTestId} = render(
+      <HomePage navigation={{replace: jest.fn()}} />,
+    );
+
+    await waitFor(() => expect(getByTestId('delete-button')).toBeTruthy());
+
+    fireEvent.press(getByTestId('delete-button'));
+
+    await waitFor(() => {
+      expect(alertSpy).toHaveBeenCalledWith('Server Error', 'Delete failed');
+    });
+  });
+
+  it('shows alert if edit fails', async () => {
+    jest.spyOn(fetchNotes, 'fetchUserNotes').mockResolvedValue([
+      {
+        shared_by_username: 'username',
+        content: 'Test note',
+        created_at: '',
+        id: 'test-note-1',
+        profile_id: 'profile1',
+        shared_by: 'user1',
+        profiles: {
+          username: 'username',
+        },
+      },
+    ]);
+
+    const alertSpy = jest.spyOn(Alert, 'alert');
+
+    const mockEdit = jest.fn(() => ({
+      eq: jest.fn(() => ({
+        select: jest.fn().mockResolvedValue({
+          data: null,
+          error: {message: 'Edit failed'},
+        }),
+      })),
+    }));
+
+    const mockFrom = jest.fn(() => ({
+      update: mockEdit,
+    }));
+
+    const supabaseMock = require('../../../../src/lib/supabase');
+    supabaseMock.supabase.from = mockFrom;
+    const {getByTestId} = render(
+      <HomePage navigation={{replace: jest.fn()}} />,
+    );
+    await waitFor(() =>
+      expect(getByTestId('edit-button-test-note-1')).toBeTruthy(),
+    );
+
+    fireEvent.press(getByTestId('edit-button-test-note-1'));
+
+    await waitFor(() => {
+      expect(alertSpy).toHaveBeenCalledWith('Server Error', 'Edit failed');
+    });
+  });
+
+  it('returns empty array if profile fetch fails', async () => {
+    jest.mocked(supabase.from).mockImplementation(
+      () =>
+        ({
+          select: jest
+            .fn()
+            .mockResolvedValue({data: null, error: {message: 'Fetch error'}}),
+        } as any),
+    );
+
+    const {getByTestId} = render(
+      <HomePage navigation={{replace: jest.fn()}} />,
+    );
+    await waitFor(() => expect(getByTestId('share-button')).toBeTruthy());
+
+    fireEvent.press(getByTestId('share-button'));
   });
 });
